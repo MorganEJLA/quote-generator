d84c88f408ae4cbe1d242f1c1b47e7621eced934
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-03-27 22:58:58 -0400

    made the UI show the quotes from the API

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -1,10 +1,18 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 // get quotes from API//
 function newQuote(){
     // pick a random quote from the array //
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    // console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 async function getQuotes(){
     const apiUrl = 'https://type.fit/api/quotes';
@@ -17,6 +25,11 @@ async function getQuotes(){
     }
 }
 
-// On Load run getQuotes function //
+// // On Load run getQuotes function //
 
 getQuotes();
+
+
+//onload
+
+// newQuote();
